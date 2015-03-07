a5f3b6ed5b4bd1da40552926f523bdf4b1be78e5
Michael Wisely <michaelwisely@gmail.com>
2015-03-07 16:25:16 -0600

    Install djangorestframework
    Add djangorestframework to buildout.cfg, pin versions down, and add it
    to settings.py/urls.py

--- auditor/auditor/urls.py
+++ auditor/auditor/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'auditor.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
+    # Built-in admin
     url(r'^admin/', include(admin.site.urls)),
+
+    # Login for rest framework's browsable API
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 )
